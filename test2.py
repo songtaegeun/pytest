2c420fd5f1b4176813284f6048b85cf144427e6d
ted2485 <xorms2375@gmail.com>
2019-06-27 13:26:03 +0900

    test code

---
+++ test2.py
@@ -0,0 +1,14 @@
+a = int(input("a = "))
+b = int(input("b = "))
+
+r = a + b
+print(a, "+", b, "=", r)
+
+r = a - b
+print(a, "-", b, "=", r)
+
+r = a * b
+print(a, "*", b, "=", r)
+
+r = a / b
+print(a, "/", b, "=", r)
